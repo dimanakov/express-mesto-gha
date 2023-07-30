e1cd992a831486e4fb82d049eb7ba49da745cfee
E-spacnvrnd <nasdaq80@mail.ru>
2023-07-30 20:13:38 +0700

    feature: add Helmet, update remove Card method

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
@@ -8,14 +9,13 @@ const { login, createUser } = require('./controllers/auth');
 const auth = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/errorHandler');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
-mongoose.connect(
-  'mongodb://127.0.0.1:27017/mestodb',
-);
+mongoose.connect(DB_URL);
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- controllers/cards.js
+++ controllers/cards.js
@@ -29,7 +29,7 @@ module.exports.removeCard = (req, res, next) => {
         next(new FOBIDDEN_403('Удаление чужих карточек невозможно'));
         return;
       }
-      Card.findByIdAndRemove(req.params.cardId)
+      Card.deleteOne(card)
         .then(() => {
           res.send({ message: 'Карточка удалена' });
         });
